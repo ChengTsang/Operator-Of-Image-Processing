6a8cddd568cb631606ecf305cff9728e96b33b62
ChengTsang <34964022+ChengTsang@users.noreply.github.com>
2018-11-19 21:38:58 +0800

    Update and rename Lapalace.py to lapalace.py

--- lapalace.py
+++ lapalace.py
@@ -11,25 +11,21 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import scipy.signal as signal
 
-
-# 拉普拉斯算子， 3*3
+# lapalace operator， 3*3
 operator = [[0,1,0],[1,-4,1],[0,1,0]]
-Lapalace_operator = np.array(operator).transpose()
+lapalace_operator = np.array(operator).transpose()
 
-# 打开图像并转化成灰度图像
+# Open the image and transform it into a grayscale image.
 image = Image.open("1.jpg").convert("L")
 image_array = np.array(image)
 
+# image and lapalace operator convolve
+image4 = signal.convolve2d(image_array,lapalace_operator,mode = "same")
 
-# 图像与拉普拉斯算子进行卷积
-image4 = signal.convolve2d(image_array,Lapalace_operator,mode = "same")
-
-# 结果转化到0-255
+# transferm the result to 0-255
 image4 = (image4/float(image4.max()))*255
          
-         
-
-# 显示图像
+# display the image
 
 plt.figure("1")
 plt.imshow(image4,cmap=cm.gray)
